084eabae9f1678932f687efa1e1caa382ce092a8
Br0k3r6 <119007986+Br0k3r6@users.noreply.github.com>
2022-12-18 11:51:53 -0500

    Add files via upload

---
+++ exfdb/exf_utils/database/clear.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm exf_utils/database/dbconfig.conf
+touch exf_utils/database/dbconfig.conf
+echo "0" >exf_utils/database/dbconfig.conf
---
+++ exfdb/exf_utils/database/dblib.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+def check_res():
+	import os
+	return 0
